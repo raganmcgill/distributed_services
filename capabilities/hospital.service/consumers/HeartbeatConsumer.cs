2f6a64fee7c05b5ec5a8cd6f9248b3568cf20eeb
Ragan McGill <ragan@themcgillfamily.co.uk>
2019-02-13 13:51:35 +0000

    hospital

---
+++ capabilities/hospital.service/consumers/HeartbeatConsumer.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using MassTransit;
+using messages.commands;
+using Newtonsoft.Json;
+
+namespace hospital.service.consumers
+{
+    internal class HeartbeatConsumer : IConsumer<Heartbeat>
+    {
+
+        public Task Consume(ConsumeContext<Heartbeat> context)
+        {
+            //ConsoleAppHelper.PrintHeader("Header.txt");
+
+            var heartBeat = context.Message;
+
+            Store(heartBeat);
+
+            //Console.WriteLine(DateTime.Now);
+
+            return Task.CompletedTask;
+        }
+
+        private void Store(Heartbeat heartBeat)
+        {
+            var subPath = $@"C:\dev\Stores\hospital\";
+
+            if (!Directory.Exists(subPath))
+            {
+                Directory.CreateDirectory(subPath);
+            }
+
+            var filename = $"{heartBeat.Name}.txt";
+
+            var serilisedHeartbeat = JsonConvert.SerializeObject(heartBeat);
+
+            var path = Path.Combine(subPath, filename);
+
+            File.WriteAllText(path, serilisedHeartbeat);
+        }
+    }
+}
---
+++ messages/commands/Heartbeat.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace messages.commands
+{
+    public interface Heartbeat
+    {
+        string Name { get; }
+
+        string Path { get; }
+
+        DateTime DateTime { get; }
+    }
+}
