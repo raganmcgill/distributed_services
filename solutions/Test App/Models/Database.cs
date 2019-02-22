1cbf7bce5b119cc4e18f13590067f055461c382c
Ragan McGill <ragan@themcgillfamily.co.uk>
2019-02-22 13:35:45 +0000

    added a web project

---
+++ solutions/Test App/Models/Database.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace dashboard.ui.Models
+{
+//    public class Database
+//    {
+//        public List<Table> Tables { get; set; }
+//        public string Name { get; set; }
+//
+//        public Database()
+//        {
+//            Tables = new List<Table>();
+//        }
+//    }
+}
